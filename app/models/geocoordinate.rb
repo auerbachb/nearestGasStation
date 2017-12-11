79ce1fa1fdaeefe1c5e3a09083654a2aafab5326
wang <phx.dn.wang@gmail.com>
2017-12-11 19:06:02 +0800

    add location_geocoordinate and gas location
    rspec the location_geocoordinate and nearest_gas_location

--- app/models/geocoordinate.rb
+++ app/models/geocoordinate.rb
@@ -4,4 +4,60 @@ class Geocoordinate
   field :city, type: String
   field :state, type: String
   field :postal_code, type: String
+
+  def self.location_geocoordinate(location)
+    google_geocoding_apikey ='AIzaSyCP_z5svlwx1ojxFeU4qDBLmNsBbv-Y-IY'
+    begin
+      RestClient.proxy = "http://127.0.0.1:8089/"
+      # use the google geocoding api load the location geocoordinate
+      res = RestClient.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=#{location[:lat]},#{location[:lng]}&key=#{google_geocoding_apikey}")
+      results = JSON.parse(res.body)["results"]
+      if results.empty?
+        return { status: -1, msg: 'not load the location geocoordinate'}
+      end
+      address = address_to_geocoordinate(results.first["address_components"])
+    rescue Exception => e
+      return { status: -2, msg: e.message}
+    end
+  end
+
+  def self.nearest_gas_station(location)
+    google_places_apikey = 'AIzaSyCjlXWxmVQjGgcbvWmoOuP_MZRucBtKPRM'
+    begin
+      # use the google map api get the nearest gas station place id
+      res = RestClient.get("https://maps.googleapis.com/maps/api/place/radarsearch/json?location=#{location[:lat]},#{location[:lng]}&radius=5000&type=gas_station&key=#{google_places_apikey}")
+      results = JSON.parse(res.body)["results"]
+      if results.empty?
+        return { status: -1, msg: 'not found any gas station'}
+      end
+      place_id = results.first["place_id"]
+      # use the google map api load the nearest gas station geocoordinate
+      res = RestClient.get("https://maps.googleapis.com/maps/api/place/details/json?placeid=#{place_id}&key=#{google_places_apikey}")
+      result = JSON.parse(res.body)["result"]["address_components"]
+      nearest_gas_station = address_to_geocoordinate(result)
+    rescue Exception => e
+      return { status: -2, msg: e.message}
+    end
+  end
+
+  private
+
+  def self.address_to_geocoordinate(address_components)
+    street_number = address_components[0]["short_name"]
+    route = address_components[1]["short_name"]
+    city = address_components[3]["long_name"]
+    state = address_components[5]["short_name"]
+    postal_code = address_components[7]["short_name"]
+    begin
+      postal_code_suffix = '-' + address_components[8]["short_name"]
+    rescue Exception => e
+      postal_code_suffix = ''
+    end
+    {
+      "streetAddress": "#{street_number} #{route}",
+      "city": city,
+      "state": state,
+      "postalCode": "#{postal_code}#{postal_code_suffix}"
+    }
+  end
 end
--- spec/models/geocoordinate_spec.rb
+++ spec/models/geocoordinate_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Geocoordinate, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'can load the location of nearest gas station' do
+    lat = 37.77801
+    lng = -122.4119076
+    result = Geocoordinate.nearest_gas_station({lat: lat, lng: lng})
+    nearest_gas_station = { streetAddress: "2300 16th St", city: "San Francisco", state: "CA", postalCode: "94103" }
+    expect(result).to eq nearest_gas_station
+  end
+
+  it 'not found the nearest gas station' do
+    lat = 30.0576504
+    lng = -124.002289
+    result = Geocoordinate.nearest_gas_station({lat: lat, lng: lng})
+    not_found = {:status=>-1, :msg=>"not found any gas station"}
+    expect(result).to eq not_found
+  end
+
+  it 'can load the location geocoordinate' do
+    lat = 37.77801
+    lng = -122.4119076
+    result = Geocoordinate.location_geocoordinate({lat: lat, lng: lng})
+    location_geocoordinate = {:streetAddress=>"1155 Mission St", :city=>"San Francisco", :state=>"CA", :postalCode=>"94103-1514"}
+    expect(result).to eq location_geocoordinate
+  end
+
+  it 'not load the location geocoordinate' do
+    lat = 30.0576504
+    lng = -124.002289
+    result = Geocoordinate.location_geocoordinate({lat: lat, lng: lng})
+    not_load = { :status=>-1, :msg=>"not load the location geocoordinate" }
+    expect(result).to eq not_load
+  end
 end
